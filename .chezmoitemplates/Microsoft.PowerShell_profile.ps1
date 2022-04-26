0cc457436282fe351426ca17fd3e0d8420f44748
satorope <40160099+satorope@users.noreply.github.com>
2022-04-26 16:18:11 +0900

    add pstemplate

--- .chezmoitemplates/Microsoft.PowerShell_profile.ps1
+++ .chezmoitemplates/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
-﻿#!/usr/bin/env pwsh
+#!/usr/bin/env pwsh
 # Import the Chocolatey Profile that contains the necessary code to enable
 # tab-completions to function for `choco`.
 # Be aware that if you are missing these lines from your profile, tab completion
