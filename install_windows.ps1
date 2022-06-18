351c2e04cf9cb4e09088c16a56605c2b78b819ae
satorope <40160099+satorope@users.noreply.github.com>
2022-06-18 11:32:02 +0900

    Add dot_nanorc Update install_windows.ps1

--- install_windows.ps1
+++ install_windows.ps1
@@ -22,3 +22,10 @@ scoop install chezmoi
 # vim-plug
 Invoke-WebRequest -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |
 New-Item $HOME/vimfiles/autoload/plug.vim -Force
+
+# AWKPATH
+# ".:/usr/local/share/awk"
+# PS [System.Environment]::SetEnvironmentVariable('AWKPATH, "Bar", 'Machine')
+# AWKLIBPATH C:\Users\tsuna\scoop\apps\gawk\current\lib\gawk\ext-3.1
+# PS [System.Environment]::SetEnvironmentVariable("AWKLIBPATH", "$HOME\scoop\apps\gawk\current\lib\gawk\ext-3.1", "User")
+
